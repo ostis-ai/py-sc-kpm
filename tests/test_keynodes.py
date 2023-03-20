27df1effd0f4506fe1ce3766f8e65e6815b8e8f1
FalseR20 <kno3.2002@gmail.com>
2023-03-20 10:50:10 +0300

    fix: rewrite deprecated client methods and types

--- tests/test_keynodes.py
+++ tests/test_keynodes.py
@@ -32,7 +32,7 @@ class KeynodesTests(BaseTestCase):
         ScKeynodes.resolve(idtf, sc_types.NODE_CONST)
         self.assertTrue(ScKeynodes.delete(idtf))
         self.assertFalse(ScKeynodes.get(idtf).is_valid())
-        self.assertRaises(InvalidValueError, ScKeynodes.__delitem__, idtf)
+        self.assertRaises(InvalidValueError, ScKeynodes.delete, idtf)
 
     def test_keynodes_initialization(self):
         self.assertRaises(TypeError, ScKeynodes)
