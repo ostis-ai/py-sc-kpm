a8e21c1db918e0df7440e4a0c2b2c9c5eb260715
FalseR20 <kno3.2002@gmail.com>
2023-08-09 11:55:54 +0300

    refactor(sc-client): update constants

--- src/sc_client/constants/common.py
+++ src/sc_client/constants/common.py
@@ -30,10 +30,43 @@ class ClientCommand(IntEnum):
     EVENTS_DESTROY = auto()
 
 
-SOURCE = "src"
-EDGE = "edge"
-TARGET = "trg"
+class Elements:
+    NODE = "node"
+    EDGE = "edge"
+    LINK = "link"
 
+
+class Types:
+    ADDR = "addr"
+    TYPE = "type"
+    ALIAS = "alias"
+    REF = "ref"
+    IDTF = "idtf"
+
+
+class CommandTypes:
+    SET = "set"
+    GET = "get"
+    RESOLVE = "resolve"
+    FIND = "find"
+    FIND_LINKS_BY_SUBSTRING = "find_links_by_substr"
+    FIND_LINKS_CONTENTS_BY_CONTENT_SUBSTRING = "find_strings_by_substr"
+    CREATE = "create"
+    DELETE = "delete"
+
+
+class ScEventType(Enum):
+    UNKNOWN = "unknown"
+    ADD_OUTGOING_EDGE = "add_outgoing_edge"
+    ADD_INGOING_EDGE = "add_ingoing_edge"
+    REMOVE_OUTGOING_EDGE = "remove_outgoing_edge"
+    REMOVE_INGOING_EDGE = "remove_ingoing_edge"
+    REMOVE_ELEMENT = "delete_element"
+    CHANGE_CONTENT = "content_change"
+
+
+SOURCE = "src"
+TARGET = "trg"
 ID = "id"
 ADDR = "addr"
 ADDRS = "addrs"
@@ -49,56 +82,12 @@ ERRORS = "errors"
 MESSAGE = "message"
 REF = "ref"
 DATA = "data"
-
 ELEMENT = "el"
-
 SCS = "scs"
 OUTPUT_STRUCTURE = "output_structure"
-
-
-class Elements:
-    NODE = "node"
-    EDGE = "edge"
-    LINK = "link"
-
-
 TYPE = "type"
-
-
-class Types:
-    ADDR = "addr"
-    TYPE = "type"
-    ALIAS = "alias"
-    REF = "ref"
-    IDTF = "idtf"
-
-
 COMMAND = "command"
-
-
-class CommandTypes:
-    SET = "set"
-    GET = "get"
-    RESOLVE = "resolve"
-    FIND = "find"
-    FIND_LINKS_BY_SUBSTRING = "find_links_by_substr"
-    FIND_LINKS_CONTENTS_BY_CONTENT_SUBSTRING = "find_strings_by_substr"
-    CREATE = "create"
-    DELETE = "delete"
-
-
 ELEMENT_TYPE = "elType"
 IDTF = "idtf"
 TEMPLATE = "templ"
 PARAMS = "params"
-IS_REQUIRED = "is_required"
-
-
-class ScEventType(Enum):
-    UNKNOWN = "unknown"
-    ADD_OUTGOING_EDGE = "add_outgoing_edge"
-    ADD_INGOING_EDGE = "add_ingoing_edge"
-    REMOVE_OUTGOING_EDGE = "remove_outgoing_edge"
-    REMOVE_INGOING_EDGE = "remove_ingoing_edge"
-    REMOVE_ELEMENT = "delete_element"
-    CHANGE_CONTENT = "content_change"
